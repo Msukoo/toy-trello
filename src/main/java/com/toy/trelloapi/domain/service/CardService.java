fea5beec925270230145524b97172e14c33f9368
Msukoo <rnalsttnn@gmail.com>
2022-02-20 16:29:46 +0900

    workListNotFoundException message로 수정

--- src/main/java/com/toy/trelloapi/domain/service/CardService.java
+++ src/main/java/com/toy/trelloapi/domain/service/CardService.java
@@ -3,6 +3,7 @@ package com.toy.trelloapi.domain.service;
 import com.toy.trelloapi.domain.dto.CardDto;
 import com.toy.trelloapi.domain.entity.Card;
 import com.toy.trelloapi.domain.entity.WorkList;
+import com.toy.trelloapi.domain.exception.WorkListNotFoundException;
 import com.toy.trelloapi.domain.repository.CardQueryRepository;
 import com.toy.trelloapi.domain.repository.CardRepository;
 import com.toy.trelloapi.domain.repository.WorkListRepository;
@@ -25,8 +26,7 @@ public class CardService {
 
         WorkList workList = workListRepository.getById(workListId);
         if(workList == null){
-            throw new NotFoundWorkListException();
-            return null;
+            throw new WorkListNotFoundException("리스트를 찾을 수 없습니다.");
         }
         return cardRepository.save(
                 Card.builder()
